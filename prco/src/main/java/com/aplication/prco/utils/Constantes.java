b502e0d5271dc6074bcb902a761817932302ccd8
JoseRodriguez2412 <j.gpe02rosarito2412@gmail.com>
2024-03-30 22:41:36 -0600

    Se agrega control de excepciones

---
+++ prco/src/main/java/com/aplication/prco/utils/Constantes.java
@@ -0,0 +1,9 @@
+package com.aplication.prco.utils;
+
+public class Constantes {
+    public static final String USUARIO_NO_EXISTENTE = "El usuario ingresado no existe";
+    public static final String SESION_NO_EXISTENTE = "La sesion ingresada no existe";
+    public static final String EXITO_NUEVA_SESION = "Nueva sesion creada con exito";
+    public static final String EXITO_REGISTRO_ASISTENCIA = "Asistencia registrada con exito";
+    public static final String EXITO_REGISTRO_USUARIO = "Usuario registrado con exito";
+}
