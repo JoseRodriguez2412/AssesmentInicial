e7b79c504ce75f87b2b9d4bae3796c1ad756f026
JoseRodriguez2412 <j.gpe02rosarito2412@gmail.com>
2024-03-30 21:51:21 -0600

    Aplicación inicial SpringBoot

---
+++ prco/src/main/java/com/aplication/prco/PrcoApplication.java
@@ -0,0 +1,13 @@
+package com.aplication.prco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrcoApplication.class, args);
+	}
+
+}
