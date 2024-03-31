8044a0c4dab27e48d8a7474745805b17be1ac9bf
JoseRodriguez2412 <j.gpe02rosarito2412@gmail.com>
2024-03-30 22:47:50 -0600

    Se agrega la sección de repositorio

---
+++ prco/src/main/java/com/aplication/prco/repository/RegistroAsistenciasRepository.java
@@ -0,0 +1,8 @@
+package com.aplication.prco.repository;
+
+import com.aplication.prco.data.RegistroAsistencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegistroAsistenciasRepository extends JpaRepository<RegistroAsistencia, Integer>{
+    
+}
---
+++ prco/src/main/java/com/aplication/prco/repository/SesionesRepository.java
@@ -0,0 +1,8 @@
+package com.aplication.prco.repository;
+
+import com.aplication.prco.data.Sesiones;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SesionesRepository extends JpaRepository<Sesiones, Integer>{
+    
+}
